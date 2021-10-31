c81e858a76e39bfd651508c2530379408d42d796
unique-pear486 <unique-pear486@users.noreply.github.com>
2021-10-31 15:59:29 +1100

    Improved model.py to support all recipes so far.

--- model.py
+++ model.py
@@ -1,17 +1,30 @@
 from __future__ import annotations
-import re
-from fractions import Fraction
+from enum import Enum
 from typing import Optional, List, Dict
-from pydantic import BaseModel, validator
-from pydantic.fields import ModelField
+from uuid import UUID
+from pydantic import BaseModel, validator, HttpUrl
 from pydantic_yaml import YamlModel
 
 from mixed import MixedNumber
 
-class Note(BaseModel):
-    note: str
+
+#
+# Enums
+#
+class FanSpeed(str, Enum):
+    off = 'Off'
+    low = 'Low'
+    high = 'High'
 
 
+class TempUnit(str, Enum):
+    celsius = 'C'
+    fahrenheit = 'F'
+
+
+#
+# Custom Classes
+#
 class Number(MixedNumber):
     "Generic number type, based on MixedNumber"
     @classmethod
@@ -20,16 +33,38 @@ class Number(MixedNumber):
 
     @classmethod
     def __modify_schema__(cls, field_schema):
-        pass
+        field_schema.update(
+            pattern='^[+-]?[0-9]* [0-9]+/?[0-9]*$',
+            examples=['1', '3/4', '2 1/3'],
+        )
 
     @classmethod
     def validate(cls, v):
         return cls(v)
 
 
+#
+# BaseModels
+#
+class Note(BaseModel):
+    note: str
+
+
+class OvenTemp(BaseModel):
+    amount: int
+    unit: TempUnit
+
+
 class Amount(BaseModel):
     amount: Number
     unit: str
+
+
+class Book(BaseModel):
+    authors: Optional[List[str]]
+    title: str
+    isbn: Optional[str]
+    notes: Optional[List[Note]]
 
 
 class Ingredient(BaseModel):
@@ -48,12 +83,19 @@ class Step(BaseModel):
 
 
 class Recipe(YamlModel):
+    recipe_uuid: Optional[UUID]
     recipe_name: str
     ingredients: List[Dict[str, Ingredient]]
     source_url: Optional[str]
     yields: Optional[List[Dict[str, str]]]
     steps: List[Step]
     preparation_time: Optional[str]
+    oven_fan: Optional[FanSpeed]
+    oven_temp: Optional[OvenTemp]
+    oven_time: Optional[str]
+    source_book: Optional[Book]
+    source_url: Optional[HttpUrl]
+    source_authors: Optional[List[str]]
 
     class Config():
         extra = 'forbid'
@@ -70,8 +112,15 @@ class Recipe(YamlModel):
 
 
 def main():
-    r = Recipe.parse_file('eg.yaml')
-    print(r)
+    from pathlib import Path
+    recipes = Path('../recipe/')
+    for filename in recipes.iterdir():
+        if filename.suffix == '.yaml':
+            print('===' + str(filename))
+            r = Recipe.parse_file(filename)
+            print(r)
+    # r = Recipe.parse_file('eg.yaml')
+    # print(r)
 
 
 if __name__ == '__main__':
